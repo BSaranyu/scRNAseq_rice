5fcc71894582de47e9221eb3a12ccb12a03f2eaa
Saranyu.m <bksaranyu@gmail.com>
2026-09-15 19:02:48 +0700

    Update 02_Data_Integration.R

--- src/02_Data_Integration.R
+++ src/02_Data_Integration.R
@@ -2,11 +2,7 @@ library(Seurat)
 library(harmony)
 library(patchwork)
 
-rn1.singlet <- readRDS(file = "normal_RN1_singlet.rds")
-rd2.singlet <- readRDS(file = "drought_RD2_singlet.rds")
-mergedDat <- merge(rn1.singlet, y = rd2.singlet, project = "Rice Leaf", merge.data = TRUE)
-names(mergedDat@meta.data)[names(mergedDat@meta.data) == "orig.ident"] <- "conditions"
-
+mergedDat <- readRDS(file = "mergedDat.rds")
 mergedDat <- SCTransform(mergedDat)
 mergedDat <- RunPCA(object = mergedDat, feature = VariableFeatures(object = mergedDat), npcs = 100)
 mergedDat <- RunHarmony(object = mergedDat, group.by.vars = 'conditions', dim = 1:75)
